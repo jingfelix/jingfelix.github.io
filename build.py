4f09bdda5afbcdc0f48da2462ee020bdfe72c82c
jingfelix <jingfelix@outlook.com>
2022-04-24 13:21:18 +0800

    [Update] 20220424

--- build.py
+++ build.py
@@ -1,43 +1,123 @@
-# import
-from articles import pson
-from shutil import copy
-from markdown import markdown
-# config
-baseHtmlPath = './static/base.html'
-articleDirPath = './articles/'
-indexPath = 'index.html'
-aboutPath = 'about.html'
-readmePath = 'README.md'
-url = r'https://jingfelix.github.io'
+try:
+    from htmlobj import *
+    from pathlib import Path
+    import markdown
+    import copy
+    import json
+except ImportError:
+    print("ImportError: Missing one or more modules.")
+    exit(1)
 
 
-# read base.html
-with open(baseHtmlPath, mode='r', encoding='utf-8') as b:
-    base = b.read()
+def viewPageIndex(article_path: str) -> str:
+    p = Path(article_path)
 
-# build articles from draft(json.html)
-for key in pson.keys():
-    with open((articleDirPath + key + '.html'), mode='w', encoding='utf-8') as f:
-        title = pson[key]['title']
-        photo = pson[key]['photo']
-        content = pson[key]['content']
-        form = pson[key]['format']
-        # 为了兼容之前用 html 制作的文章，提供了可改选项
-        if form == 'md':
-            content = markdown(content).replace(
-                '$', '<div class="blank"></div>').replace('<ul>', '').replace('</ul>', '')
+    if p.is_file():
+        return None
 
-        template = base.replace('[[TITLE]]', title).replace(
-            '[[PHOTO]]', photo).replace('[[CONTENT]]', content).replace('[[URL]]', url)
+    main: HtmlDom = HtmlDom()
+    div: HtmlDom = HtmlDom(
+        element="div", _class="mdui-col-xs-12 mdui-typo-subheading para"
+    )
+    i: HtmlDom = HtmlDom(
+        element="i", _class="mdui-icon material-icons", content="chevron_right"
+    )
+    em: HtmlDom = HtmlDom(element="em")
+    a: HtmlDomA = HtmlDomA()
 
-        f.write(template)
-        print('{0} build complete!'.format(title))
-    pass
+    em.addChildren(a)
+    div.addChildrenList([i, em])
 
-# copy index.html & about.html
-copy(articleDirPath + indexPath, indexPath)
-copy(articleDirPath + aboutPath, aboutPath)
+    for name in list(p.glob("*.md")):
+        name = (
+            str(name).replace(".md", "").replace(str(article_path), "").replace("/", "")
+        )
+        # TODO: 修改name变量使其不包含路径
 
-# build README.md
-with open(readmePath, mode='w', encoding='utf-8') as r:
-    r.write(pson['about']['content'].replace('[[URL]]', url))
+        _div = copy.deepcopy(div)
+        _a = (_div.find("em")[0]).find("a")[0]
+        _a.content = name
+
+        # TODO: important, an undefined father path should be used here.
+        _a.href = "/articles/" + name + ".html"
+
+        main.addChildren(_div)
+
+    return main.toString()
+
+
+def viewPagePost(article_path: str, template_file_path: str, pic_dic: dict) -> bool:
+
+    p = Path(article_path)
+
+    # get template html content
+    with open(template_file_path, "r") as f:
+        template_content = f.read()
+
+    # get article content
+    for name in list(p.glob("*.md")):
+        name = str(
+            str(name).replace(".md", "").replace(str(article_path), "").replace("/", "")
+        )
+
+        if name in pic_dic:
+            photo_to_replace = pic_dic[name]
+        else:
+            photo_to_replace = "https://api.vvhan.com/api/bing?type=sj"
+
+        with open(article_path + "/" + name + ".md", "r") as md_file:
+            markdown_content = md_file.read()
+
+            # convert markdown to html
+            markdown_to_replace = markdown.markdown(markdown_content)
+
+        with open(article_path + "/" + name + ".html", "w") as html_file:
+            html_file.write(
+                template_content.replace("<!--MARKDOWN-->", markdown_to_replace)
+                .replace("<!--TITLE-->", name)
+                .replace("<!--MAINPHOTO-->", photo_to_replace)
+            )
+    return True
+
+
+def main():
+
+    current_path = Path.cwd()
+    article_path = current_path / "articles"
+    template_file_path = current_path / "templates" / "template.html"
+
+    with open(template_file_path, "r") as f:
+        template_file_content = f.read()
+
+    content_to_replace = viewPageIndex(article_path)
+    new_index = (
+        template_file_content.replace("<!--CONTENT-->", content_to_replace)
+        .replace("<!--TITLE-->", "归档")
+        .replace(
+            "<!--MAINPHOTO-->",
+            "https://pic3.zhimg.com/50/v2-f8b5afa8767a3c1b2bd126f8ba1b42b6_r.jpg",
+        )
+    )
+
+    with open(current_path / "index.html", "w", encoding="utf-8") as f:
+        f.write(new_index)
+
+    with open(current_path / "pic.json", "r") as f:
+        pic_dic = json.load(f)
+
+    with open(article_path / "关于.md", "r") as f1:
+        with open(current_path / "README.md", "w+") as f2:
+            f2.write(f1.read())
+
+    if viewPagePost(str(article_path), str(template_file_path), pic_dic) is True:
+        print("Build success.")
+    else:
+        print("Build failed.")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt: Build interrupted.")
+        exit(1)
